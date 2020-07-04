b0f2591ccf71e869908aa201d1e6bcc55e37bc8f
Jayson Nguyen <jvnyxc@umsystem.edu>
2020-07-04 06:48:31 -0500

    Updating Employee.h

--- Employee.h
+++ Employee.h
@@ -6,5 +6,23 @@ using namespace std;
 class Employee
 {
 public:
+	Employee();
+	Employee(int waitTime, int retainingTime, string name);
 
+	void setWaitTime(int waitTime);
+	void setRetainingTime(int retainingTime);
+	void setEmployeeName(string name);
+
+	int getWaitTime();
+	int getRetainingTime();
+	string getName();
+	int getPriority();
+
+	void updatePriority();
+
+private:
+	int waitTime;
+	int retainingTime;
+	string employeeName;
+	int priority;
 };
