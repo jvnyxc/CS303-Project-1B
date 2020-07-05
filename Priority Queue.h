4d1a4d512130e56b4d54c589437c2a616f4ae609
Jayson Nguyen <jvnyxc@umsystem.edu>
2020-07-05 00:20:10 -0500

    Updating Priority Queue.h

--- Priority Queue.h
+++ Priority Queue.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <iostream>
+#include "Employee.h"
+using namespace std;
+
+struct Node
+{
+	Employee data;
+	struct Node* next;
+};
+
+class PriorityQueue
+{
+public:
+	PriorityQueue();
+	void push(Employee* employee);
+	bool isEmpty();
+	void pop();
+
+private:
+	int numberOfEmployee;
+	Node* head;
+};
