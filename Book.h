a0eedaba5de4fdc3308bf5c24d555449e3ce0b74
Jayson Nguyen <jvnyxc@umsystem.edu>
2020-07-04 23:53:06 -0500

    Updating Book.h

--- Book.h
+++ Book.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <iostream>
+#include<string>
+#include "Date.h"
+#include "Employee.h"
+using namespace std;
+
+class Book
+{
+public:
+	Book();
+	Book(string bookName);
+
+	void setBookName(string bookName);
+	void setDateStart(Date dateStart);
+	void setDateEnd(Date dateEnd);
+
+	string getBookName();
+	Date getDateStart();
+	Date getDateEnd();
+
+	bool isArchived();
+	void addEmployeeToQueue(Employee* user);
+	void removeHighPriority();
+
+private:
+	string bookName;
+	Date dateStart;
+	Date dateEnd;
+	bool archived;
+};
